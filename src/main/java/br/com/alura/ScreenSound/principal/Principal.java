52a804ff25b02b3a2745d05f83b79d7f458ece59
Giovanni Morais <giovannimo1@yahoo.com>
2024-10-30 16:25:32 -0300

    feat: Implementando database

--- src/main/java/br/com/alura/ScreenSound/principal/Principal.java
+++ src/main/java/br/com/alura/ScreenSound/principal/Principal.java
@@ -10,14 +10,14 @@ public class Principal {
 
         while (opcao!= 9) {
             var menu = """
-                    *** Screen Sound Músicas ***                    
-                                        
+                    *** Screen Sound Músicas ***
+                    
                     1- Cadastrar artistas
                     2- Cadastrar músicas
                     3- Listar músicas
                     4- Buscar músicas por artistas
                     5- Pesquisar dados sobre um artista
-                                    
+                    
                     9 - Sair
                     """;
 
